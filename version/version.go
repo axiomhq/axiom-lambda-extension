1460e234ad65d289b442e951c968bdb5b15f873d
Islam Shehata <islam@axiom.co>
2023-10-11 14:19:01 +0300

    prepare version 6

--- version/version.go
+++ version/version.go
@@ -1,7 +1,7 @@
 package version
 
 // manually set constant version
-const version string = "v5"
+const version string = "v6"
 
 // Get returns the Go module version of the axiom-go module.
 func Get() string {
