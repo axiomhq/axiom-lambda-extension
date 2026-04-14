b06282de6005da6599afbfc2893c515c5c715252
Islam Shehata <islam@axiom.co>
2026-04-14 15:50:36 +0200

    chore: bump version

--- version/version.go
+++ version/version.go
@@ -1,7 +1,7 @@
 package version
 
 // manually set constant version
-const version string = "v13"
+const version string = "v14"
 
 // Get returns the Go module version of the axiom-go module.
 func Get() string {
